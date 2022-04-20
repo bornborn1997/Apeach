fe7414330860c8324ba8c0aaa62845dc3d9abc38
bornborn1997 <bornborn1997@naver.com>
2022-04-20 19:33:02 +0900

    npm 재설치

--- src/routers/Tabstackscreen.js
+++ src/routers/Tabstackscreen.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+
 import Homescreen from "../screens/HomeScreen/Homescreen"
 import Chatroom from '../screens/HomeScreen/Chatroomscreen';
 import Community from '../screens/HomeScreen/Comunityscreen';
@@ -12,7 +15,44 @@ const TabStack = createBottomTabNavigator ();
 
 const tabstackscreen = () => {
     return(
-        <TabStack.Navigator screenOptions={{headerShown : false}}>
+        <TabStack.Navigator  screenOptions={({ route  }) => ({
+           headerShown: false,
+           
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+              let rn = route.name
+  
+              if (rn === 'home') {
+                iconName = focused  ? 'home' : 'home-outline';
+              } else if (rn === 'Community') {
+                iconName = focused ? 'ios-list' : 'ios-list';
+              }else if (rn === 'Chat') {
+                iconName = focused ? 'chatbubbles' : 'chatbubbles-outline';
+              }else if (rn === 'Userlist') {
+                iconName = focused ? 'people' : 'people-outline';
+              }else if (rn === 'Profile') {
+                iconName = focused ? 'person' : 'person-outline';
+              }
+  
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: '#b7b4df',
+            inactiveTintColor: '#9b59b6',
+            style : {
+              position : 'absolute',
+              bottom : 25,
+              left : 20,
+              right : 20,
+              elevation : 0,
+              backgroundcolor : '#ffffff',
+              borderRadius : 15,
+              height : 90,
+              
+            }
+            
+          }}>
          <TabStack.Screen name="Community" component={Community}/>
             <TabStack.Screen name="Chat" component={Chatroom}/>
             <TabStack.Screen name="home" component={Homescreen}/>
